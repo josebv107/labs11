0e0eecef937213a70d43df8168fb18b4e80e6c28
josebv107 <josebv107@gmail.com>
2026-06-21 17:25:57 -0500

    feat: clase Cafe usa HashMap para precios por tamaño N, M y G

--- src/Cafe.java
+++ src/Cafe.java
@@ -1,19 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Cafe implements Bebida {
     String descripcion;
-    double precioBase;
+    Map<Tamano, Double> precios;
+    Tamano tamano;
 
-
-    public Cafe(String descripcion, double precioBase) {
+    public Cafe(String descripcion, double precioN, double precioM, double precioG) {
         this.descripcion = descripcion;
-        this.precioBase = precioBase;
+        this.precios = new HashMap<>();
+        this.precios.put(Tamano.NORMAL, precioN);
+        this.precios.put(Tamano.MEDIANO, precioM);
+        this.precios.put(Tamano.GRANDE, precioG);
+        this.tamano = Tamano.NORMAL;
     }
 
     @Override
     public String getDescripcion() {
-        return descripcion;
+        return descripcion + " (" + tamano + ")";
     }
+
     @Override
     public double getCosto() {
-        return precioBase;
+        return precios.get(tamano);
+    }
+
+    @Override
+    public Tamano getTamano() {
+        return tamano;
+    }
+
+    @Override
+    public void setTamano(Tamano tamano) {
+        this.tamano = tamano;
     }
 }
