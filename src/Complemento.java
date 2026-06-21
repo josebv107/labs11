70c1dd084eaa313d13ffda48d8ad1b8d5631bbb1
josebv107 <josebv107@gmail.com>
2026-06-21 17:34:04 -0500

    feat: clase Complemento usa HashMap para precios por tamaño N, M y G

--- src/Complemento.java
+++ src/Complemento.java
@@ -1,15 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Complemento implements Bebida {
     protected Bebida bebida;
+    protected Map<Tamano, Double> precios;
 
-    public Complemento(Bebida bebida) {
+    public Complemento(Bebida bebida, double precioN, double precioM, double precioG) {
         this.bebida = bebida;
+        this.precios = new HashMap<>();
+        this.precios.put(Tamano.NORMAL, precioN);
+        this.precios.put(Tamano.MEDIANO, precioM);
+        this.precios.put(Tamano.GRANDE, precioG);
     }
 
     public String getDescripcion() {
         return bebida.getDescripcion();
     }
 
+
     public double getCosto() {
-        return bebida.getCosto();
+        return bebida.getCosto() + precios.get(bebida.getTamano());
+    }
+
+    @Override
+    public Tamano getTamano() {
+        return bebida.getTamano();
+    }
+
+    @Override
+    public void setTamano(Tamano tamano) {
+        bebida.setTamano(tamano);
     }
 }
